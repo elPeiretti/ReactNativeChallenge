5126afa699d90e634aa69dfec87edc19830c0073
elPeiretti <tomaspeiretti@gmail.com>
2023-02-27 17:42:51 -0300

    add style to Button.js

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 const Button = (props) => {
     return (
-        <TouchableOpacity>
-            <Text>{props.text}</Text>
+        <TouchableOpacity style={[styles.button, props.color && {backgroundColor: props.color}]}>
+            <Text style={styles.text}>{props.text}</Text>
         </TouchableOpacity>
     );
 }
+
+const styles = StyleSheet.create({
+    button:{
+        borderRadius: 10,
+        borderWidth: 3,
+        height: 70,
+        width: 300,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#d6f9ff'
+    },
+    text:{
+        fontSize: 36,
+        fontFamily: 'sans-serif-medium',
+        color: '#5A5A5A'
+    }
+});
 
 export default Button;

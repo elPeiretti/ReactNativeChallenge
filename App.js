149d9085c6dd7a98d2f0e1a71d04143b577866f2
elPeiretti <tomaspeiretti@gmail.com>
2023-02-27 18:33:18 -0300

    add styles to MainScreen

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import MainScreen from './components/ui/MainScreen';
 
 function App() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{flex:1}}>
       <MainScreen/>
     </SafeAreaView>
   );
--- components/ui/MainScreen.js
+++ components/ui/MainScreen.js
@@ -5,16 +5,64 @@ import Button from './Button';
 const MainScreen = () => {
 
     return (
-        <View>
-            <Button text='Normal'/>
-            <Button text='Time-trial'/>
-            <Button text='Exit'/>
+        <View style={styles.main}>
+            <View style={styles.helpContainer}>
+                <TouchableOpacity style={styles.helpButton}>
+                    <Text style={{color:"#000000", fontSize: 22}}>?</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>Sudoku</Text>
+                <Text style={styles.signature}> By elPeiretti</Text>
+            </View>
+            <View>
+                <Button text='Normal' style={{alignSelf: 'center'}}/>
+            </View>
+            <View style={{paddingTop: 10}}>
+                <Button text='Time-trial' style={{alignSelf: 'center'}}/>
+            </View>
+            <View style={{paddingTop: 200}}>
+                <Button text='Exit' color='#ffd6d6' style={{alignSelf: 'center'}}/>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-
+    main:{
+        flex: 1,
+        flexDirection: 'column',
+        position: 'relative'
+    },
+    helpContainer:{
+        width:'100%', 
+        paddingEnd: 10, 
+        paddingTop: 10
+    },
+    helpButton:{
+        width: 40,
+        height: 40,        
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        borderWidth: 2,
+        backgroundColor: '#ffe8aa',
+        alignSelf: 'flex-end'
+    },
+    titleContainer:{
+        paddingVertical: 50,
+        alignItems: 'center',
+    },
+    title:{
+        fontSize: 72,
+        fontFamily: 'monospace',
+        fontWeight: 'bold',
+        color: '#000000',
+    },
+    signature:{
+        alignSelf: 'flex-end',
+        paddingEnd: 70
+    }
 });
 
 export default MainScreen;

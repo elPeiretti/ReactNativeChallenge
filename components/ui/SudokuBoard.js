bc6a4559857dc096d0f3f46eae1192834844baa8
elPeiretti <tomaspeiretti@gmail.com>
2023-02-28 13:33:13 -0300

    cell paints itself when is selected

--- components/ui/SudokuBoard.js
+++ components/ui/SudokuBoard.js
@@ -1,36 +1,48 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
 
 const SudokuBoard = () => {
 
     const [solution, setSolution] = useState();
 
     const cell = {
-        value: 1,
-        notes: [],
+        i: 99,
+        j: 99,
+        value: 2,
+        notes: [[]],
         visible: false,
         prefilled: false
     }
+
     const [matrix, setMatrix] = useState([
-        [cell, cell, cell, cell, cell, cell, cell, cell, cell],
-        [cell, cell, cell, cell, cell, cell, cell, cell, cell],
-        [cell, cell, cell, cell, cell, cell, cell, cell, cell],
-        [cell, cell, cell, cell, cell, cell, cell, cell, cell],
-        [cell, cell, cell, cell, cell, cell, cell, cell, cell],
-        [cell, cell, cell, cell, cell, cell, cell, cell, cell],
-        [cell, cell, cell, cell, cell, cell, cell, cell, cell],
-        [cell, cell, cell, cell, cell, cell, cell, cell, cell],
-        [cell, cell, cell, cell, cell, cell, cell, cell, cell],
+        [{...cell, i:0, j:0}, {...cell, i:0, j:1}, {...cell, i:0, j:2}, {...cell, i:0, j:3}, {...cell, i:0, j:4}, {...cell, i:0, j:5}, {...cell, i:0, j:6}, {...cell, i:0, j:7}, {...cell, i:0, j:8}],
+        [{...cell, i:1, j:0}, {...cell, i:1, j:1}, {...cell, i:1, j:2}, {...cell, i:1, j:3}, {...cell, i:1, j:4}, {...cell, i:1, j:5}, {...cell, i:1, j:6}, {...cell, i:1, j:7}, {...cell, i:1, j:8}],
+        [{...cell, i:2, j:0}, {...cell, i:2, j:1}, {...cell, i:2, j:2}, {...cell, i:2, j:3}, {...cell, i:2, j:4}, {...cell, i:2, j:5}, {...cell, i:2, j:6}, {...cell, i:2, j:7}, {...cell, i:2, j:8}],
+        [{...cell, i:3, j:0}, {...cell, i:3, j:1}, {...cell, i:3, j:2}, {...cell, i:3, j:3}, {...cell, i:3, j:4}, {...cell, i:3, j:5}, {...cell, i:3, j:6}, {...cell, i:3, j:7}, {...cell, i:3, j:8}],
+        [{...cell, i:4, j:0}, {...cell, i:4, j:1}, {...cell, i:4, j:2}, {...cell, i:4, j:3}, {...cell, i:4, j:4}, {...cell, i:4, j:5}, {...cell, i:4, j:6}, {...cell, i:4, j:7}, {...cell, i:4, j:8}],
+        [{...cell, i:5, j:0}, {...cell, i:5, j:1}, {...cell, i:5, j:2}, {...cell, i:5, j:3}, {...cell, i:5, j:4}, {...cell, i:5, j:5}, {...cell, i:5, j:6}, {...cell, i:5, j:7}, {...cell, i:5, j:8}],
+        [{...cell, i:6, j:0}, {...cell, i:6, j:1}, {...cell, i:6, j:2}, {...cell, i:6, j:3}, {...cell, i:6, j:4}, {...cell, i:6, j:5}, {...cell, i:6, j:6}, {...cell, i:6, j:7}, {...cell, i:6, j:8}],
+        [{...cell, i:7, j:0}, {...cell, i:7, j:1}, {...cell, i:7, j:2}, {...cell, i:7, j:3}, {...cell, i:7, j:4}, {...cell, i:7, j:5}, {...cell, i:7, j:6}, {...cell, i:7, j:7}, {...cell, i:7, j:8}],
+        [{...cell, i:8, j:0}, {...cell, i:8, j:1}, {...cell, i:8, j:2}, {...cell, i:8, j:3}, {...cell, i:8, j:4}, {...cell, i:8, j:5}, {...cell, i:8, j:6}, {...cell, i:8, j:7}, {...cell, i:8, j:8}],
     ]);
+
+    const [selectedCell, setSelectedCell] = useState({});
+
+    function selectCell(cell){
+        setSelectedCell(cell);
+        console.log(cell);
+    }
 
     return (
         <View style={styles.board}>
             {matrix.map(row => (
                 <View style={styles.boardRow}>
                     {row.map(col => (
-                        <View style={styles.boardCell}>
-                            <Text>{col.value}</Text>
-                        </View>
+                        <Pressable key={[col.i, col.j]}
+                            style={[styles.boardCell, selectedCell === col && {backgroundColor: 'yellow'}]}
+                            onPress={()=>{selectCell(col)}}>
+                            <Text style={{fontSize:22}}>{col.value}</Text>
+                        </Pressable>
                     ))}
                 </View>
             ))}
@@ -43,14 +55,21 @@ export default SudokuBoard;
 const styles = StyleSheet.create({
     board:{
         flexDirection: 'column',
-        borderWidth: 1
+        borderWidth: 1,
+        alignItems: 'center',
     },
     boardRow:{
         flexDirection: 'row',
-        borderWidth: 1,
-        justifyContent: 'space-evenly'
+        borderWidth: 0.5,
+        backgroundColor: '#0000FF'
     },
     boardCell:{
-        borderWidth: 3,
+        borderLeftWidth: 0.5,
+        borderRightWidth: 0.5,
+        width: 40,
+        height: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#00FFFF'
     }
 });

7a2ff234936624595c06417a705154ed5c146e77
elPeiretti <tomaspeiretti@gmail.com>
2023-02-28 13:52:02 -0300

    add grid lines to board

--- components/ui/SudokuBoard.js
+++ components/ui/SudokuBoard.js
@@ -6,14 +6,15 @@ const SudokuBoard = () => {
     const [solution, setSolution] = useState();
 
     const cell = {
-        i: 99,
-        j: 99,
+        i: -1,
+        j: -1,
         value: 2,
         notes: [[]],
         visible: false,
         prefilled: false
     }
 
+    //best thing that came to my mind without googling hehe
     const [matrix, setMatrix] = useState([
         [{...cell, i:0, j:0}, {...cell, i:0, j:1}, {...cell, i:0, j:2}, {...cell, i:0, j:3}, {...cell, i:0, j:4}, {...cell, i:0, j:5}, {...cell, i:0, j:6}, {...cell, i:0, j:7}, {...cell, i:0, j:8}],
         [{...cell, i:1, j:0}, {...cell, i:1, j:1}, {...cell, i:1, j:2}, {...cell, i:1, j:3}, {...cell, i:1, j:4}, {...cell, i:1, j:5}, {...cell, i:1, j:6}, {...cell, i:1, j:7}, {...cell, i:1, j:8}],
@@ -36,10 +37,17 @@ const SudokuBoard = () => {
     return (
         <View style={styles.board}>
             {matrix.map(row => (
-                <View style={styles.boardRow}>
+                <View style={styles.boardRow} key={row[0].i}>
                     {row.map(col => (
                         <Pressable key={[col.i, col.j]}
-                            style={[styles.boardCell, selectedCell === col && {backgroundColor: 'yellow'}]}
+                            style={[
+                                styles.boardCell,
+                                col.j == 0 && {borderLeftWidth: 2},
+                                col.i == 0 && {borderTopWidth: 2},
+                                (col.i+1)%3 == 0 && {borderBottomWidth: 2},
+                                (col.j+1)%3 == 0 && {borderRightWidth: 2},
+                                selectedCell === col && {backgroundColor: 'yellow'},
+                            ]}
                             onPress={()=>{selectCell(col)}}>
                             <Text style={{fontSize:22}}>{col.value}</Text>
                         </Pressable>
@@ -60,12 +68,10 @@ const styles = StyleSheet.create({
     },
     boardRow:{
         flexDirection: 'row',
-        borderWidth: 0.5,
         backgroundColor: '#0000FF'
     },
     boardCell:{
-        borderLeftWidth: 0.5,
-        borderRightWidth: 0.5,
+        borderWidth: 0.5,
         width: 40,
         height: 40,
         alignItems: 'center',

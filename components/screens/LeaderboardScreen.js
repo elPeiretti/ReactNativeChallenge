5edd742ebf36ff35525747ee906e65078eefe3da
elPeiretti <tomaspeiretti@gmail.com>
2023-03-09 17:52:01 -0300

    implement LeaderboardScreen

--- components/screens/LeaderboardScreen.js
+++ components/screens/LeaderboardScreen.js
@@ -1,12 +1,49 @@
-import React from "react";
-import { View } from "react-native"
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native"
+import { getEasyLeaderboard } from "../../persistence/repository/LeaderboardRepository";
 
 const LeaderboardScreen = () => {
-    return (
-        <View>
 
-        </View>
+    const [easyLb, setEasyLb] = useState([]);
+    const [mediumLb, setMediumLb] = useState([]);
+    const [hardLb, setHardLb] = useState([]);
+
+    useEffect(() => {
+        const loadEasy = async () => {
+            return await getEasyLeaderboard();
+        }
+        loadEasy().then(r => {
+            //ok this works
+            return Object.keys(r).map((key) => [key, Object.keys(r[key]).map(s => r[key][s])[0]])})
+            .then(setEasyLb);
+    }, []);
+
+    return (
+        <SafeAreaView>
+            <ScrollView>
+                <View style={{alignItems: 'center'}}>
+                    <View>
+                        <Text style={styles.title}>Easy</Text>
+                        {console.log(easyLb)}
+                    </View>
+                    <View>
+                        <Text style={styles.title}>Medium</Text>
+                    </View>
+                    <View>
+                        <Text style={styles.title}>Hard</Text>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
 export default LeaderboardScreen;
+
+const styles = StyleSheet.create({
+    title:{
+        fontSize: 42,
+        fontFamily: 'monospace',
+        fontWeight: 'bold'
+    }
+});
--- persistence/repository/LeaderboardRepository.js
+++ persistence/repository/LeaderboardRepository.js
@@ -5,11 +5,10 @@ const checkResponseStatus = (response) => {
     throw new Error(response.statusText);
 }
 
-const getEasyLeaderboard = async () => {
-    var top10 = fetch('127.0.0.1:8000/ranking/easy/')
+export async function getEasyLeaderboard() {
+    var top10 = fetch('http://127.0.0.1:8000/ranking/easy/')
         .then(checkResponseStatus)
-        .then(data => JSON.parse(data))
-        .catch(err => console.log('request failed', err));
-
+        .catch(err => console.log('request failed', err))
+        .then(r => {return r.json()});
         return top10;
 }
